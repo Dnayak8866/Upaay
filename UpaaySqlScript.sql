0cbb5374ff9658a0605e7e3c31aa1aa732608c9f
Dnayak8866 <nayakdharmesh2@gmail.com>
2025-02-23 20:05:43 +0530

    update script

--- UpaaySqlScript.sql
+++ UpaaySqlScript.sql
@@ -1,5 +1,21 @@
 /* connecting to "Upaay" database... */
 
+-- Database: Upaay
+
+-- DROP DATABASE IF EXISTS "Upaay";
+
+-- Check if not exists condition
+
+CREATE DATABASE "Upaay"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_India.1252'
+    LC_CTYPE = 'English_India.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
 
 
 CREATE TABLE Users(
