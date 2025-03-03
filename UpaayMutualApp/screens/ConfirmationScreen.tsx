06e8a3bd2f5442471fb7c61937de3c969439c257
Chandan Sahu <cs154896@gmail.com>
2025-03-04 03:32:56 +0530

    fixed UI issues

--- UpaayMutualApp/screens/ConfirmationScreen.tsx
+++ UpaayMutualApp/screens/ConfirmationScreen.tsx
@@ -1,21 +1,25 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { LinearGradient } from "expo-linear-gradient"
 
-type ConfirmationProps ={
-    onBackToLogin : () => void
+
+type ConfirmationProps = {
+  onBackToLogin: () => void
 }
-const ConfirmationScreen = ({onBackToLogin}: ConfirmationProps) => {
+const ConfirmationScreen = ({ onBackToLogin }: ConfirmationProps) => {
   return (
-    <View className='flex-1 justify-center items-center px-6 bg-green-50'>
-      <Image source={require('../assets/images/congratulation.png')}/>
-      <Text className="text-3xl font-extrabold text-gray-800 mt-4">Congratulations!</Text>
-      <Text className="text-gray-600 text-center mt-2">Your new password has been updated successfully.</Text>
-      <TouchableOpacity className="mt-8" onPress={onBackToLogin}>
-        <Text className="text-primary font-bold font-poppins">Sign in here</Text>
-      </TouchableOpacity>
-      <StatusBar style='dark'/>
-    </View>
+    <LinearGradient colors={["#EBFBF7", "transparent", "#EBFBF7"]} style={{ flex: 1 }}>
+      <View className='flex-1 justify-center items-center px-6 bg-green-50'>
+        <Image source={require('../assets/images/congratulation.png')} />
+        <Text className="text-3xl font-extrabold text-[#292D32] font-poppins mt-4">Congratulations!</Text>
+        <Text className="text-[#777F90] text-center mt-2 font-poppins">Your new password has been updated successfully.</Text>
+        <TouchableOpacity className="mt-8" onPress={onBackToLogin}>
+          <Text className="text-primary font-bold font-poppins">Sign in here</Text>
+        </TouchableOpacity>
+        <StatusBar style='dark' />
+      </View>
+    </LinearGradient>
   )
 }
 
